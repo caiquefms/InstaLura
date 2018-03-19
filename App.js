c92be9d2afc733a2f42e041f11e96a5d8799047a
Caiquefms <caiquef.ms@gmail.com>
2018-03-19 15:16:48 -0300

    Primeiro Commit

--- App.js
+++ App.js
@@ -9,50 +9,47 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  Dimensions,
+  ScrollView,
+  FlatList
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+
+const width  = Dimensions.get("screen").width;
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
+  render() {  
+    const fotos = [
+      {id:1,usuario:'Caique'},
+      {id:2,usuario:'Joao'},
+      {id:3,usuario:'Carlos'}
+    ];
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      // <ScrollView style={{marginTop:20}}>
+      //   {fotos.map(foto=>
+      //     <View key={foto.id  }>
+      //       <Text>{foto.usuario}</Text>
+      //       <Image source={require('./resources/img/alura.png')} style={{width:width, height:width}} />
+      //     </View>
+      //   )}
+      // </ScrollView>
+      <FlatList style={{marginTop:20}}
+        //keyExtractor = {item =>''+item.id}
+        keyExtractor = {item =>String(item.id)}
+        data = {fotos}
+        renderItem ={({item})=>
+          <View>
+            <Text>{item.usuario}</Text>
+            <Image source={require('./resources/img/alura.png')} style={{width:width, height:width}} />
+          </View>
+        }
+
+      />
+
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
